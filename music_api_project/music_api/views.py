c9a2796adfbf8dfd2cbe38caa8b0ad350642d6d4
Micah <airrenno@yahoo.com>
2023-02-18 23:51:34 -0600

    Updated the api paths + Updated the models.

--- music_api_project/music_api/views.py
+++ music_api_project/music_api/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view 
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import SongModelSerializer
+from .models import SongModel
+
+@api_view(['GET',])
+def song_list(request):
+
+    if request.method == 'GET':
+         song = SongModel.objects.all()
+         
+         
+
+
+
+
+
 
 # Create your views here.
--- music_api_project/music_api_project/settings.py
+++ music_api_project/music_api_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'music_api',
     'rest_framework', 
+    'music_api_project',
 ]
 
 MIDDLEWARE = [
